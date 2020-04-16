ae1313b74d613842a0b5aebb551f9f085f02adcf
Leah Winters <leah.winters24@gmail.com>
2020-04-16 18:50:36 -0400

    Create skeleton for user, hotel, and manager classes

---
+++ src/hotel.js
@@ -0,0 +1,26 @@
+class Hotel {
+  constructor(allUserData, allRoomsData, allBookingData) {
+    this.allUserData = allUserData;
+    // this.allAvailableRooms = [];
+    this.allRoomsData = allRoomsData;
+    this.allBookingData = allBookingData;
+    this.allRooms = [];
+    this.allGuests = [];
+    this.allCurrentBookings = [];
+  }
+
+  filterRoomsByType() {
+
+  }
+
+  filterAvailableRoomsBySelectedDate() {
+
+  }
+
+  calculateRevenue() {
+
+  }
+}
+
+
+export default Hotel;
